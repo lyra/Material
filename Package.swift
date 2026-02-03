2b1b24030ec1eb266134c4e7718f3829809a57f0
MageSantana <33928559+MageSantana@users.noreply.github.com>
2026-02-03 15:53:52 +0100

    Update Package.swift
    Signed-off-by: MageSantana <33928559+MageSantana@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LyraMaterial",
-            url: "https://raw.githubusercontent.com/lyra/Material/1.0.7/LyraMaterial.xcframework.zip",
+            url: "https://raw.githubusercontent.com/lyra/Material/1.0.8/LyraMaterial.xcframework.zip",
             checksum: "8e96770fd7377348d869fc5c25c7745ce1470994848b52f13a70a2d32504add9"
         ),
         .target(
