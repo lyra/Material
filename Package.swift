a7ae2fe833c1b604b18cdc72ee5222fb1533e685
MageSantana <33928559+MageSantana@users.noreply.github.com>
2026-06-24 19:38:36 +0200

    Update Package.swift
    Signed-off-by: MageSantana <33928559+MageSantana@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "LyraMaterial",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v26)
     ],
     products: [
         .library(
@@ -15,13 +15,13 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/lyra/Motion.git",
-            exact: "4.0.3"
+            exact: "4.0.4"
         )
     ],
     targets: [
         .binaryTarget(
             name: "LyraMaterial",
-            url: "https://raw.githubusercontent.com/lyra/Material/1.0.9/LyraMaterial.xcframework.zip",
+            url: "https://raw.githubusercontent.com/lyra/Material/1.0.10/LyraMaterial.xcframework.zip",
             checksum: "8e96770fd7377348d869fc5c25c7745ce1470994848b52f13a70a2d32504add9"
         ),
         .target(
