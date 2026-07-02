bfc33887aeb1a4e94f5c6d02e9e3183ac64b31cc
MageSantana <33928559+MageSantana@users.noreply.github.com>
2026-07-02 16:06:23 +0200

    Update Package.swift
    Set min iOS to 15.0
    
    Signed-off-by: MageSantana <33928559+MageSantana@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.4
 import PackageDescription
 
 let package = Package(
     name: "LyraMaterial",
     platforms: [
-        .iOS(.v26)
+        .iOS(.v15)
     ],
     products: [
         .library(
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LyraMaterial",
-            url: "https://raw.githubusercontent.com/lyra/Material/1.0.10/LyraMaterial.xcframework.zip",
+            url: "https://raw.githubusercontent.com/lyra/Material/1.0.11/LyraMaterial.xcframework.zip",
             checksum: "8e96770fd7377348d869fc5c25c7745ce1470994848b52f13a70a2d32504add9"
         ),
         .target(
